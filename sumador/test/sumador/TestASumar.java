456f25888991587f87333fc6e8ab2b944e5fbcd6
Juan Bautista Penalva Perez <usu@usu-PC>
2019-02-28 22:19:58 +0100

    merge segunda prueba

--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -10,10 +10,16 @@ class TestASumar {
 	 * comprueba si 5 es igual a 5 = 5
 	 */
 	@Test
-	 public void testUnaCifra() {
-		        ASumar sumi = new ASumar("5");
-		        String resultado = sumi.mostrar();
-		        assertEquals("5 = 5",resultado );
-		    }
+	public void testUnaCifra() {
+		ASumar sumi = new ASumar("5");
+		String resultado = sumi.mostrar();
+		assertEquals("5 = 5", resultado);
 	}
 
+	@Test
+	public void testNegativo() {
+		ASumar sumi = new ASumar("-1");
+		String resultado = sumi.mostrar();
+		assertEquals("", resultado);
+	}
+}
