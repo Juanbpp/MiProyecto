bbd0271540aa6bcc7d02d1444034fb40aac04862
Juan Bautista Penalva Perez <usu@usu-PC>
2019-03-04 17:50:11 +0100

    Prueba 3 realizada.

--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -15,11 +15,23 @@ class TestASumar {
 		String resultado = sumi.mostrar();
 		assertEquals("5 = 5", resultado);
 	}
-
+/**
+ * comprueba si el numero es negativo y es ""
+ */
 	@Test
 	public void testNegativo() {
 		ASumar sumi = new ASumar("-1");
 		String resultado = sumi.mostrar();
 		assertEquals("", resultado);
 	}
+/**
+ * comprueba si el numero es mayor de 9 y devuelve una cadena y su valor total.
+ */
+	@Test
+	public void testmasde1digito() {
+		ASumar sumi = new ASumar("12");
+		String resultado = sumi.mostrar();
+		assertEquals("1 + 2 = 3", resultado);
+	}
+
 }
