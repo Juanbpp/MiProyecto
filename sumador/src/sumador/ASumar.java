619958f9aef7283ce88206751cd54a65221a15a4
Juan Bautista Penalva Perez <usu@usu-PC>
2019-03-04 18:12:55 +0100

    Merge branch 'master' into Prueba3

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,27 +1,59 @@
 package sumador;
 
-import static org.junit.Assert.assertEquals;
-
+/**
+ * 
+ * @author Juan Bautista Penalva Perez
+ *
+ */
 public class ASumar {
-
 	private String valor_inicial;
-
+	/**
+	 * 
+	 * @param string valor a calcular
+	 */
 	public ASumar(String string) {
-		 this.valor_inicial = string;
-		
-		// TODO Auto-generated constructor stub
+		this.valor_inicial = string;
+	}
+	/**
+	 * Si el numero tiene una longitud de 1 devuelve el mismo numero
+	 * Si el numero es menor de 0 devuelve ""
+	 * Si el numero es mayor de 9 devuelve una cadena con su suma.
+	 * @return devuelve el valor de mostrar
+	 */
+	public String mostrar() {
+		String numero = valor_inicial;
+		if (numero.length() == 1) {
+			return (numero + " = " + numero);
+		} else if (Integer.parseInt(numero) < 0) {
+			return ("");
+		} else if (Integer.parseInt(numero) > 9) {
+			String suma = "";
+			for (int i = 0; i < valor_inicial.length(); i++) {
+				String digito = valor_inicial.substring(i, i + 1);
+				if (i == 0) {
+					suma = suma + digito;
+				} else {
+					suma = suma + " + " + digito;
+				}
+			}
+			numero = suma + " = " + total();
+			return numero;
+		}
+		return numero;
 	}
 /**
  * 
- * @return devuelve valor si es un numero de un digito
+ * @return devuelve la suma de los digitos.
  */
-	  public String mostrar() {
-	        String numero = valor_inicial;
-	        if (numero.length() == 1) {
-	            return (numero + " = " + numero);
-	        } else if (Integer.parseInt(numero) < 0){
-			return (""); }
-			return numero;
-	}		
+	public int total() {
+		int suma = 0;
 
+		for (int i = 0; i < valor_inicial.length(); i++) {
+			// Cadena de un dígito.
+			String digito = valor_inicial.substring(i, i + 1);
+			suma = suma + Integer.parseInt(digito);
+		}
+		return suma;
+
+	}
 }
